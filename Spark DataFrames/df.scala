8c337ca1f2ab8ecf97a0198d0c54f47e40088d29
dagiel <patryk.dagiel@gmail.com>
2017-11-03 17:07:13 +0100

    Update 03.11

--- Spark DataFrames/df.scala
+++ Spark DataFrames/df.scala
@@ -3,13 +3,29 @@ import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
 
 val df = spark.read.option("header","true").option("inferSchema", "true").csv("CitiGroup2006_2008")
-df.columns
-df.head(5)
+// df.columns
+// df.head(5)
+//
+// for(row <- df.head(5)){
+//   println(row)
+// }
+//
+// df.describe().show()
+// val df2 = df.withColumn("HighPlusLow", df("High")+df("Low"))
+// df2.printSchema()
 
-for(row <- df.head(5)){
-  println(row)
-}
+df.printSchema()
 
-df.describe().show()
-val df2 = df.withColumn("HighPlusLow", df("High")+df("Low"))
-df2.printSchema()
+import spark.implicits._
+
+df.filter($"Close">480).show()
+df.filter("Close > 480").show()
+
+df.filter($"Close" < 480 && $"High" < 480).show()
+val CH_low = df.filter($"Close" < 480 && $"High" < 480).collect()
+val CH_low_count = df.filter($"Close" < 480 && $"High" < 480).count()
+
+df.filter($"High"===484.40).show()
+df.filter("High = 484.40").show()
+
+df.select(corr("High","Low")).show()
