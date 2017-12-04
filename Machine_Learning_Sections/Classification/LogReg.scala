aa7566e3bf0bff6f5c7f46464cdab569e777032e
dagiel <patryk.dagiel@gmail.com>
2017-12-04 15:08:45 +0100

    Log-regresion, final training

--- Machine_Learning_Sections/Classification/LogReg.scala
+++ Machine_Learning_Sections/Classification/LogReg.scala
@@ -18,8 +18,40 @@ for(ind <- Range(1,colnames.length)){
   println(firstrow(ind))
   println("\n")
 }
-
-val logreddataall = (data.select(data("Survived").as("label"), $"Pclass", $"Name",
+val logregdataall = (data.select(data("Survived").as("label"), $"Pclass", $"Name",
     $"Sex", $"Age", $"SibSp", $"Parch", $"Fare", $"Embarked"))
 
 val logregdata = logregdataall.na.drop()
+
+import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer, VectorIndexer, OneHotEncoder}
+import org.apache.spark.ml.linalg.Vectors
+
+// Converting String into numerical values
+val genderIndexer = new StringIndexer().setInputCol("Sex").setOutputCol("SexIndex")
+val embarkIndexer = new StringIndexer().setInputCol("Embarked").setOutputCol("EmbarkIndex")
+
+// Convert  Numerical Values into one hot encoding 0 or 1
+val genderEncoder = new OneHotEncoder().setInputCol("SexIndex").setOutputCol("SexVec")
+val embarkEncoder = new OneHotEncoder().setInputCol("EmbarkIndex").setOutputCol("EmbarkVec")
+
+// (Label, features)
+val assembler = (new VectorAssembler()
+                  .setInputCols(Array("Pclass","SexVec","Age","SibSp","Parch","Fare","EmbarkVec"))
+                  .setOutputCol("features"))
+
+val Array(training, test) = logregdata.randomSplit(Array(0.7,0.3), seed=12345)
+
+import org.apache.spark.ml.Pipeline
+val lr = new LogisticRegression()
+
+val pipeline = new Pipeline().setStages(Array(genderIndexer, embarkIndexer, genderEncoder, embarkEncoder, assembler, lr))
+
+val model = pipeline.fit(training)
+
+val results = model.transform(test)
+
+// MODEL EVALUATION
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+val predictionAndLabels = results.select($"prediction",$"label").as[(Double,Double)].rdd
+
+val metrics = new MulticlassMetrics(predictionAndLabels)
