41faadbb1876a8fd68d52cdc1d5f1355c8d23107
dagiel <patryk.dagiel@gmail.com>
2017-12-05 16:12:28 +0100

    Project, part I

--- Machine_Learning_Sections/Classification/LogRegProject.scala
+++ Machine_Learning_Sections/Classification/LogRegProject.scala
@@ -26,14 +26,16 @@
 //////////////////////
 
 // Import SparkSession and Logistic Regression
-
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.sql.SparkSession
 // Optional: Use the following code below to set the Error reporting
-
+import org.apache.log4j._
 // Create a Spark Session
-
+val spark = SparkSession.builder().getOrCreate()
 // Use Spark to read in the Advertising csv file.
-
+val data = spark.read.option("header","true").option("inferSchema","true").format("csv").load("advertising.csv")
 // Print the Schema of the DataFrame
+data.printSchema()
 
 ///////////////////////
 /// Display Data /////
@@ -41,6 +43,14 @@
 
 // Print out a sample row of the data (multiple ways to do this)
 
+val colnames = data.columns
+val firstrow = data.head(1)(0)
+println("\n")
+println("Example Data Row")
+for(ind <- Range(1,colnames.length)){
+  println(colnames(ind))
+  println(firstrow(ind))
+}
 
 ////////////////////////////////////////////////////
 //// Setting Up DataFrame for Machine Learning ////
