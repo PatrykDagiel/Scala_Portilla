ae87ca55adf9e9afdbf65f8c65d02413a7859cac
Patryk Dagiel <patryk.dagiel@gmail.com>
2017-12-05 19:24:56 +0100

    Project part II

--- Machine_Learning_Sections/Classification/LogRegProject.scala
+++ Machine_Learning_Sections/Classification/LogRegProject.scala
@@ -60,40 +60,45 @@ for(ind <- Range(1,colnames.length)){
 //    - Rename the Clicked on Ad column to "label"
 //    - Grab the following columns "Daily Time Spent on Site","Age","Area Income","Daily Internet Usage","Timestamp","Male"
 //    - Create a new column called Hour from the Timestamp containing the Hour of the click
+val timedata = data.withColumn("Hour",hour(data("Timestamp")))
 
-
-
+val logregdata = ( timedata.select(data("Clicked on Ad").as("label"),
+                   $"Daily Time Spent on Site", $"Age", $"Area Income", $"Daily Internet Usage", $"Hour", $"Male" ))
 // Import VectorAssembler and Vectors
-
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 // Create a new VectorAssembler object called assembler for the feature
 // columns as the input Set the output column to be called features
-
+val assembler = ( new VectorAssembler().setInputCols(
+                Array("Daily Time Spent on Site", "Age", "Area Income", "Daily Internet Usage", "Hour", "Male")).setOutputCol("features"))
 
 // Use randomSplit to create a train test split of 70/30
-
+val Array(training, test) = logregdata.randomSplit(Array(0.7,0.3),seed=12345)
 
 ///////////////////////////////
 // Set Up the Pipeline ///////
 /////////////////////////////
-
+import org.apache.spark.ml.Pipeline
 // Import Pipeline
 // Create a new LogisticRegression object called lr
-
+val lr = new LogisticRegression()
 // Create a new pipeline with the stages: assembler, lr
-
+val pipeline = new Pipeline().setStages(Array(assembler,lr))
 // Fit the pipeline to training set.
-
+val model = pipeline.fit(training)
 
 // Get Results on Test Set with transform
-
+val results = model.transform(test)
 ////////////////////////////////////
 //// MODEL EVALUATION /////////////
 //////////////////////////////////
 
 // For Metrics and Evaluation import MulticlassMetrics
-
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 // Convert the test results to an RDD using .as and .rdd
-
+val predictionAndLabels = results.select($"prediction",$"label").as[(Double,Double)].rdd
 // Instantiate a new MulticlassMetrics object
-
+val metrics = new MulticlassMetrics(predictionAndLabels)
 // Print out the Confusion matrix
+println("Confusion Matrix:")
+println(metrics.confusionMatrix)
