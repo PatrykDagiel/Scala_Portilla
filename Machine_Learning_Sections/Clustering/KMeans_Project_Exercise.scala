72cbd5e99d5baefadff91416aab1225ecda48f7d
dagiel <patryk.dagiel@gmail.com>
2017-12-20 14:19:14 +0100

    Project Done for clustering and KMeans algorithm

--- Machine_Learning_Sections/Clustering/KMeans_Project_Exercise.scala
+++ Machine_Learning_Sections/Clustering/KMeans_Project_Exercise.scala
@@ -35,25 +35,27 @@ import org.apache.spark.ml.clustering.KMeans
 // Import Kmeans clustering Algorithm
 val dataset = spark.read.option("header", "true").option("inferSchema","true").csv("Wholesale customers data.csv")
 // Load the Wholesale Customers Data
-val feature_data = dataset.select()
+val feature_data = dataset.select($"Fresh", $"Milk", $"Grocery", $"Frozen", $"Detergents_Paper", $"Delicassen")
 // Select the following columns for the training set:
 // Fresh, Milk, Grocery, Frozen, Detergents_Paper, Delicassen
 // Cal this new subset feature_data
-
-
 // Import VectorAssembler and Vectors
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 
 // Create a new VectorAssembler object called assembler for the feature
 // columns as the input Set the output column to be called features
 // Remember there is no Label column
+val assembler = new VectorAssembler().setInputCols(Array("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")).setOutputCol("features")
 
 // Use the assembler object to transform the feature_data
 // Call this new data training_data
-
+val training_data = assembler.transform(feature_data).select("features")
 // Create a Kmeans Model with K=3
-
+val kmeans = new KMeans().setK(4)
 // Fit that model to the training_data
-
+val model = kmeans.fit(training_data)
 // Evaluate clustering by computing Within Set Sum of Squared Errors.
-
+val WSSSE = model.computeCost(training_data)
 // Shows the result.
+println(s"The Within Set Sum of Squared errors was: ${WSSSE}")
